07ec8cc79842a963385760543cb8ba86968a8e35
aliyildirim <neoxp.pro@gmail.com>
2023-10-05 21:19:11 +0300

    Oku
    Scheduller olarak eklersen rdp licence resetler

---
+++ rdpreset.ps1
@@ -0,0 +1,8 @@
+﻿# The target registry key's full path.
+$keyPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
+
+# Pass the value name pattern to Remove-ItemProperty's -Name parameter.
+# Remove `-WhatIf` if the preview suggests that the operation works as intended.
+Remove-ItemProperty -Path $keyPath -Name '*L$RTMTIME*'
+
+shutdown /r  /t 60 /c "Lütfen açık olan ve çalışmakta olduğunuz ofis/netsis gibi uygulamalarınızı veri kaynı yaşamamak için kaydedip çıkış yapınız. 60 sn içerisinde uzak sunucuyu yeniden başlatma devreye girecektir."
---
+++ user member.ps1
@@ -0,0 +1,52 @@
+﻿$users = Get-ADUser -Filter * -Properties physicalDeliveryOfficeName, MemberOf, DistinguishedName
+$groups = Get-ADGroup -Filter *
+
+$logFolderPath = "C:\User Group Logs"
+
+foreach ($user in $users) {
+    $userOffice = $user.physicalDeliveryOfficeName
+    $memberOfGroups = $user.MemberOf | ForEach-Object { ($_ -split ',')[0] -replace '^CN=' }
+    
+    $userGroupsString = $groups | Where-Object { $userOffice -eq $_.Name } | ForEach-Object { $_.Name }
+    $userDN = $user.DistinguishedName
+    $ouParts = ($userDN -split ',OU=')[1..$($userDN -split ',OU=').Length] | ForEach-Object { ($_ -replace '^OU=', '') }
+    $ouPath = $ouParts -join ' / '
+    
+    if ($userGroupsString.Count -gt 0) {
+        $isMember = $false
+        foreach ($group in $userGroupsString) {
+            if ($user.MemberOf -contains (Get-ADGroup $group).DistinguishedName) {
+                $isMember = $true
+                break
+            }
+        }
+        
+        if (-not $isMember) {
+            $result = Add-ADGroupMember -Identity $userGroupsString -Members $user.SamAccountName -ErrorAction SilentlyContinue
+            if ($result -eq $null) {
+                $duyuruAdded = $false
+                if ($memberOfGroups -notcontains "Duyuru") {
+                    Add-ADGroupMember -Identity "Duyuru" -Members $user.SamAccountName -ErrorAction SilentlyContinue
+                    Add-ADGroupMember -Identity "Genel Duyuru" -Members $user.SamAccountName -ErrorAction SilentlyContinue
+                    Start-ADSyncSyncCycle -PolicyType Delta
+                    $duyuruAdded = $true
+                    
+                }
+                
+                $logMessage = "--------------------------------------------------------------------------------------------------------" + "`n"
+                $logMessage += "Tarih / Saat : $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" + "`n"
+                $logMessage += "Hedef Kullanıcı : $($user.SamAccountName)" + "`n"
+                $logMessage += "Hedef Kullanıcı OU : $ouPath" + "`n"
+                $logMessage += "Üye Olduğu Gruplar : $($memberOfGroups -join ', ')" + "`n"
+                $logMessage += "Üye Edildiği Grup : $($userGroupsString -join ', '), " + $(if ($duyuruAdded) { "Duyuru" } else { "" }) + "`n"
+                $logMessage += "--------------------------------------------------------------------------------------------------------" + "`n"
+                
+                $logFileName = "$($user.SamAccountName) - $(Get-Date -Format 'dd-MM-yyyy HH-mm').txt"
+                $logFilePath = Join-Path -Path $logFolderPath -ChildPath $logFileName
+                $logMessage | Out-File -FilePath $logFilePath -Encoding UTF8
+            } else {
+                $logMessage = "Hata: $($result.Exception.Message)" + "`n"
+            }
+         }
+    }
+}
